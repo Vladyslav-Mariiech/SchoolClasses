fee3f6d4b75dc4a8df8b3bc4c0719e5bfd252411
kravetso <okravets.info@gmail.com>
2025-07-10 20:55:05 +0300

    migrations files added

---
+++ app/migrations/20250710_create_users_table.php
@@ -0,0 +1,13 @@
+<?php
+
+use app\core\Database;
+
+Database::getInstance()->query("
+    CREATE TABLE IF NOT EXISTS users (
+        id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        login VARCHAR(20) NOT NULL UNIQUE,
+        password VARCHAR(255) NOT NULL,
+        email VARCHAR(100) NOT NULL UNIQUE
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+");
+
