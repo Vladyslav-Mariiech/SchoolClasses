e9455ffe0c326cf331b1fb4ffadab9ec2b5225cb
kravetso <okravets.info@gmail.com>
2025-07-10 20:53:56 +0300

    migrate file added

--- app/migrate.php
+++ app/migrate.php
@@ -1,1 +1,10 @@
 <?php
+require_once '..' . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'cred.php';
+require_once 'core/Database.php';
+require_once 'core/MigrationManager.php';
+
+use app\core\MigrationManager;
+
+$migrationsPath = '..' . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'migrations';
+$manager = new MigrationManager($migrationsPath);
+$manager->migrate();
