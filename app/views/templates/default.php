4d194069122e5c2d8d0863945e75834f44301d8b
VolodymyrZin <vl.zinych@gmail.com>
2025-07-11 22:17:56 +0300

    template default add

---
+++ app/views/templates/default.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="uk">
+    <head>
+        <meta charset="UTF-8">
+        <title>{{ title }}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../../../public/css/style.css">
+    </head>
+    <body>
+        <header>
+            <h1>{{ header }}</h1>
+        </header>
+        <main>
+            {{ content }}
+        </main>
+        <footer>
+            <p>Наша школа пропонує сучасне навчання програмуванню, веб-технологіям і проєктному мисленню. Запрошуємо!</p>
+        </footer>
+    </body>
+</html>
