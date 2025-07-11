82f36acfefc4c7ee42bd200ab618735ee03a3fb2
kravetso <okravets.info@gmail.com>
2025-07-11 12:04:29 +0300

    alter migrations files  changed

--- app/migrations/20250711_2_alter_classes_table.php
+++ app/migrations/20250711_2_alter_classes_table.php
@@ -5,7 +5,11 @@ use app\core\Database;
 Database::getInstance()->query("
     ALTER TABLE classes 
     ADD COLUMN owner_id BIGINT UNSIGNED NOT NULL;
+");
+
+Database::getInstance()->query("
     ALTER TABLE classes 
     ADD CONSTRAINT fk_classes_owner_id FOREIGN KEY (owner_id) REFERENCES users(id);
 ");
+
 
