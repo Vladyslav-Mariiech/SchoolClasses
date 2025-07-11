4f00ab47f762ba054862a2a909d6fec83afb9a10
Stas0802 <stasshulga6@gamail.com>
2025-07-11 15:34:01 +0300

    Add class UserController

---
+++ app/controllers/UserController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace app\controllers;
+
+use app\core\Session;
+use app\models\UserModel;
+use app\validations\ValidateUser;
+
+class UserController
+{
+    protected UserModel $userModel;
+
+    protected ValidateUser $validate;
+
+    public function __construct()
+    {
+        $this->userModel = new UserModel();
+        $this->validate = new ValidateUser();
+    }
+
+    /**
+     * Add new user
+     * @return void
+     */
+    public function add(): void
+    {
+        $user = [
+            'login' => $_POST['login'],
+            'password' => $_POST['password'],
+            'email' => $_POST['email'],
+            ];
+        $errors = $this->validate->userValidate($user);
+        if(!empty($errors)){
+            Session::setSession('errors', $errors);
+            return;
+        }
+        $this->userModel->add($user);
+    }
+
+    /**
+     * Finds a user by login name
+     * @param $name
+     * @return void
+     */
+    public function find($name): void
+    {
+        if(empty($name)){
+            Session::setSession('error', 'login not found');
+            return;
+        }
+        $userLogin = $this->userModel->find($name);
+        var_dump($userLogin);
+    }
+
+}
