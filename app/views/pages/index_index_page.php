03a345372565c2771bc39713564b0e33e71c275f
VolodymyrZin <vl.zinych@gmail.com>
2025-07-11 22:18:37 +0300

    index_page add

---
+++ app/views/pages/index_index_page.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="uk">
+    <head>
+        <meta charset="UTF-8">
+        <title>Головна</title>
+        <link rel="stylesheet" href="../../../public/css/style.css">
+    </head>
+    <body>
+        <header>
+            <h1>ГОЛОВНА СТОРІНКА</h1>
+        </header>
+        <main>
+            <div class="left-block">
+                <a href="index_index_page.php">
+                    <img src="../../../public/images/logo.png" alt="Логотип школи" class="logo">
+                </a>
+            </div>
+            <div class="form-block">
+                <button onclick="location.href='index_register_page.php'" class="register-btn">Реєстрація</button>
+                <form>
+                    <h2>Вхід</h2>
+                    <label>
+                        Логін: <br>
+                        <input type="text" name="login">
+                    </label><br>
+                    <label>
+                        Пароль: <br>
+                        <input type="password" name="password">
+                    </label><br><br>
+                    <button type="submit">Підтвердити</button>
+                    <button type="reset">Відмінити</button>
+                </form>
+            </div>
+        </main>
+        <footer>
+            <p>Наша школа пропонує сучасне навчання програмуванню, веб-технологіям і проєктному мисленню. Запрошуємо!</p>
+        </footer>
+    </body>
+</html>
