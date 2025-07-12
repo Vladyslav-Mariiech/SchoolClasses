905bf90809cc61b4590a284ddc4701641955e5ef
Vadym Dehtiarenko <degavv@gmail.com>
2025-07-12 11:46:44 +0300

    Pages Fixed files paths and refactor

--- app/views/pages/index_createGroup_page.php
+++ app/views/pages/index_createGroup_page.php
@@ -1,28 +1,18 @@
-<!DOCTYPE html>
-<html lang="uk">
-    <head>
-        <meta charset="UTF-8">
-        <title>Запросити до групи</title>
-        <link rel="stylesheet" href="../../../public/css/style.css">
-    </head>
-    <body>
-        <header>
-            <h1>Створення групи</h1>
-        </header>
-        <main>
-            <div class="form-block">
-                <form>
-                    <label>
-                        Назва групи: <br>
-                        <input type="text" name="name">
-                    </label><br><br>
-                    <button type="submit">ОК</button>
-                    <button type="reset">Відмінити</button>
-                </form>
-            </div>
-        </main>
-        <footer>
-            <p>Наша школа пропонує сучасне навчання програмуванню, веб-технологіям і проєктному мисленню. Запрошуємо!</p>
-        </footer>
-    </body>
-</html>
+<header>
+    <h1>Створення групи</h1>
+</header>
+<main>
+    <div class="form-block">
+        <form>
+            <label>
+                Назва групи: <br>
+                <input type="text" name="name">
+            </label><br><br>
+            <button type="submit">ОК</button>
+            <button type="reset">Відмінити</button>
+        </form>
+    </div>
+</main>
+<footer>
+    <p>Наша школа пропонує сучасне навчання програмуванню, веб-технологіям і проєктному мисленню. Запрошуємо!</p>
+</footer>
--- app/views/pages/index_inviteToGroup_page.php
+++ app/views/pages/index_inviteToGroup_page.php
@@ -1,32 +1,22 @@
-<!DOCTYPE html>
-<html lang="uk">
-    <head>
-        <meta charset="UTF-8">
-        <title>Запросити до групи</title>
-        <link rel="stylesheet" href="../../../public/css/style.css">
-    </head>
-    <body>
-        <header>
-            <h1>Запрошення до групи</h1>
-        </header>
-        <main>
-            <div class="form-block">
-                <form>
-                    <label>
-                        Логін користувача: <br>
-                        <input type="text" name="login">
-                    </label><br>
-                    <label>
-                        Посилання на групу: <br>
-                        <input type="text" name="link">
-                    </label><br><br>
-                    <button type="submit">ОК</button>
-                    <button type="reset">Відмінити</button>
-                </form>
-            </div>
-        </main>
-        <footer>
-            <p>Наша школа пропонує сучасне навчання програмуванню, веб-технологіям і проєктному мисленню. Запрошуємо!</p>
-        </footer>
-    </body>
-</html>
+<header>
+    <h1>Запрошення до групи</h1>
+</header>
+<main>
+    <div class="form-block">
+        <form>
+            <label>
+                Логін користувача: <br>
+                <input type="text" name="login">
+            </label><br>
+            <label>
+                Посилання на групу: <br>
+                <input type="text" name="link">
+            </label><br><br>
+            <button type="submit">ОК</button>
+            <button type="reset">Відмінити</button>
+        </form>
+    </div>
+</main>
+<footer>
+    <p>Наша школа пропонує сучасне навчання програмуванню, веб-технологіям і проєктному мисленню. Запрошуємо!</p>
+</footer>
