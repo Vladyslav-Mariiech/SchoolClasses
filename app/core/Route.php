720b4fa5fd525525936c8788c4a6592fa57c9cd0
dega <mail@gmail.com>
2025-07-15 11:44:42 +0300

    Route index method refactoring

--- app/core/Route.php
+++ app/core/Route.php
@@ -2,6 +2,11 @@
 
 namespace app\core;
 
+use app\exceptions\HttpException;
+use app\exceptions\HttpNotFoundException;
+use app\exceptions\HttpWrongMethodExeption;
+
+
 class Route
 {
     protected const ROUTES = [
@@ -10,22 +15,17 @@ class Route
             'controller' => self::DEFAULT_CONTROLLER,
             'action' => self::DEFAULT_ACTION,
         ],
-        '/index/index/' => [
-            'method' => 'GET',
-            'controller' => self::DEFAULT_CONTROLLER,
-            'action' => self::DEFAULT_ACTION,
-        ],
-        '/class/invite/' => [
+        '/class/invite' => [
             'method' => 'GET',
             'controller' => 'Class',
             'action' => 'showInvite',
         ],
-        '/class/join/' => [
+        '/class/join' => [
             'method' => 'GET',
             'controller' => 'Class',
             'action' => 'join',
         ],
-        '/class/add/' => [
+        '/class/add' => [
             'method' => 'GET',
             'controller' => 'Class',
             'action' => 'add',
@@ -33,9 +33,8 @@ class Route
         '/class' => [
             'method' => 'GET',
             'controller' => 'Class',
-            'action' => self::DEFAULT_ACTION,
         ],
-        '/index/registerPage/' => [
+        '/index/registerPage' => [
             'method' => 'GET',
             'controller' => 'Index',
             'action' => 'register',
@@ -59,41 +58,43 @@ class Route
 
     public function __construct()
     {
-        $this->init();
+        try{
+            $this->init();
+        } catch (HttpException $e) {
+            exit ($e->getMessage());
+        }
+
     }
 
     public function init(): void
     {
-        $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+        $uri = rtrim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/') ?: '/';
 
         if (!array_key_exists($uri, self::ROUTES)) {
-            $this->notFound();
+            throw new HttpNotFoundException();
         }
 
         $route = self::ROUTES[$uri];
 
-        if (
-            $_SERVER['REQUEST_METHOD'] !== $route['method']
-        ) {
-            Response::status(405);
-            exit('Метод не дозволено');
+        if ($_SERVER['REQUEST_METHOD'] !== $route['method']) {
+            throw new HttpWrongMethodExeption();
         }
 
-        $controllerClass = 'app\controllers\\' . $route['controller'] . 'Controller';
+        $controllerName = $route['controller'];
+        $action = $route['action'] ?? self::DEFAULT_ACTION;
+
+        $controllerClass = 'app\controllers\\' . ucfirst($controllerName) . 'Controller';
+
         if (!class_exists($controllerClass)) {
-            $this->notFound();
+            throw new HttpNotFoundException();
         }
-        $action = $route['action'];
-        $controller = new $controllerClass();
-        if (!method_exists($controller, $action)) {
-            $this->notFound();
-        }
-        $controller->$action();
-    }
 
-    protected function notFound()
-    {
-        Response::status(404);
-        exit('Не знайдено');
+        $controller = new $controllerClass();
+
+        if (!method_exists($controller, $action)) {
+            throw new HttpNotFoundException();
+        }
+
+        $controller->$action();
     }
 }
