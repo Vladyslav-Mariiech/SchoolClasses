907465799f26f3ef2df94e444f88d7a59f25a408
kravetso <okravets.info@gmail.com>
2025-07-18 00:26:14 +0300

    store() fixed

--- app/models/SubmissionModel.php
+++ app/models/SubmissionModel.php
@@ -18,11 +18,7 @@ class SubmissionModel extends BaseModel
      */
     public function getByUserId(int $id): array
     {
-        $stmt ="
-            SELECT 
-                sbmsns.id, asnmts.due_date,
-                sbmsns.grade, 
-                CASE WHEN CURRENT_TIMESTAMP() > asnmts.due_date OR sbmsns.grade = 0 THEN 'Not Passed' ELSE 'Passed' END AS status
+        $stmt ="SELECT sbmsns.id, asnmts.due_date,sbmsns.grade, CASE WHEN CURRENT_TIMESTAMP() > asnmts.due_date OR sbmsns.grade = 0 THEN 'Not Passed' ELSE 'Passed' END AS status
             FROM submissions sbmsns 
             INNER JOIN assignments asnmts ON sbmsns.assignment_id = asnmts.id
             WHERE user_id = ?";
