3db2f3de79fde83c47362f4dd85bccbc21f3be03
kravetso <okravets.info@gmail.com>
2025-07-18 00:07:25 +0300

    const ERRORMESSAGE added

--- app/config.php
+++ app/config.php
@@ -1,2 +1,8 @@
 <?php
 const SITE_NAME = 'Site Name';
+
+const ERRORS_MESSAGES = [
+    'Please fill in all the fields.',
+    'User with this login already exists.',
+    'An error occurred while registering the user.'
+];
