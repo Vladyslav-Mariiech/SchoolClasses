5a7703536852ae90d82fa9e8256d5977cfa3c546
Stas0802 <stasshulga6@gamail.com>
2025-07-21 08:02:39 +0300

    change indexController

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -2,8 +2,10 @@
 
 namespace app\controllers;
 
+use app\core\Session;
 use app\core\View;
 use app\controllers\AuthController;
+use app\services\AuthService;
 
 
 class IndexController{
@@ -12,22 +14,48 @@ class IndexController{
     {
         $this->view = new View();
     }
-    public function index()
+
+    /**
+     * Handler home page
+     * @return void
+     */
+    public function index(): void
     {
+        $errors = Session::getSession('errors');
+        Session::deleteSession('errors');
         $this->view->render('index_index', [
             'title' => 'Головна',
+            'errors' => $errors,
         ]);
     }
-    public function register() {
+
+    /**
+     * registration new user
+     * @return void
+     */
+    public function register(): void
+    {
+        $errors = Session::getSession('errors');
+        Session::deleteSession('errors');
         $this->view->render('index_register', [
-           'title' => 'Реєстрація',
+           'title' => 'Реєстрація', 'errors' => $errors,
         ]);
     }
-    public function myGroups() {
-        \app\controllers\AuthController::checkAccess();
+
+    /**
+     * Displays a page with the groups that the currently logged in user is a member of.
+     * @return void
+     */
+    public function myGroups(): void
+    {
+        AuthService::requireAuth();
+        $login = AuthService::user()['login'];
+        $userId = AuthService::userId();
 
         $this->view->render('index_myGroups', [
             'title' => 'MyGroups',
+            'login' => $login,
+            'id' => $userId,
         ]);
     }
 }
