44e2e112415b168d752cbfd888ad81d0f7707f40
Stas0802 <stasshulga6@gamail.com>
2025-07-21 08:00:04 +0300

    add php

--- app/views/pages/index_register_page.php
+++ app/views/pages/index_register_page.php
@@ -1,27 +1,49 @@
 <header>
-    <h1>РЕЄСТРАЦІЯ</h1>
+	<h1>РЕЄСТРАЦІЯ</h1>
 </header>
 <main>
-    <div class="form-block">
-        <form method="post" action="/auth/register">
-            <label>
-                Email: <br>
-                <input type="email" name="email">
-            </label><br>
-            <label>
-                Логін: <br>
-                <input type="text" name="login">
-            </label><br>
-            <label>
-                Пароль: <br>
-                <input type="password" name="password">
-            </label><br>
-            <label>
-                Підтвердіть пароль: <br>
-                <input type="password" name="confirm">
-            </label><br><br>
-            <button type="submit">Підтвердити</button>
-            <button type="button" onclick="location.href='/'">Відмінити</button>
-        </form>
-    </div>
+
+
+	<div class="form-block">
+		<form method="post" action="/user/add">
+            <?php
+            if(isset($errors['common'])): ?>
+				<div><?=$errors['common']?></div>
+            <?php endif; ?>
+            <?php if(isset($errors['email'])): ?>
+				<div class="error"><?=$errors['email']?></div>
+            <?php endif; ?>
+			<label>
+				Email: <br>
+				<input type="email" name="email"
+					   class="<?= isset($errors) ? 'error-input' : ''?>">
+			</label>
+            <?php if(isset($errors['login'])): ?>
+				<div class="error"><?=$errors['login']?></div>
+            <?php endif; ?>
+			<label>
+				Логін:
+				<input type="text" name="login"
+					   class="<?= isset($errors) ? 'error-input' : ''?>"
+			</label>
+            <?php if(isset($errors['password'])): ?>
+				<div class="error"><?=$errors['password']?></div>
+            <?php endif; ?>
+			<label>
+				Пароль:
+				<input type="password" name="password"
+					   class="<?= isset($errors) ? 'error-input' : ''?>">
+			</label>
+            <?php if(isset($errors['passConfirm'])): ?>
+				<div class="error"><?=$errors['passConfirm']?></div>
+            <?php endif; ?>
+			<label>
+				Підтвердіть пароль:
+				<input type="password" name="passConfirm" class="<?= isset($errors) ? 'error-input' : ''?>">
+			</label>
+
+			<button type="submit">Підтвердити</button>
+			<button type="button" onclick="location.href='/'">Відмінити</button>
+		</form>
+	</div>
 </main>
