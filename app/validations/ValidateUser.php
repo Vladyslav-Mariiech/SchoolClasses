39f0143bd392ec880f06d121edf075fd02ef5db2
Stas0802 <stasshulga6@gamail.com>
2025-07-21 08:05:46 +0300

    change ValidateUser

--- app/validations/ValidateUser.php
+++ app/validations/ValidateUser.php
@@ -2,6 +2,8 @@
 
 namespace app\validations;
 
+use app\models\UserModel;
+
 class ValidateUser
 {
     /**
@@ -14,18 +16,28 @@ class ValidateUser
         $errors = [];
         if(empty($data['login']) || empty($data['password']) || empty($data['email'])){
             $errors['common'] = 'Все поля должны быть заполнены';
-        }else{
+            return $errors;
+        }
             if(mb_strlen($data['login']) < 3){
                 $errors['login'] = 'Логин должен быть больше 3 символов';
             }
             if(mb_strlen($data['password']) < 6){
                 $errors['password'] = 'Пароль должен быть больше 6 символов';
             }
+            if(!empty($data['passConfirm']) && $data['passConfirm'] !== $data['password']){
+                $errors['passConfirm'] = 'Пароль не совпадает';
+            }
             if(!filter_var($data['email'], FILTER_VALIDATE_EMAIL)){
                 $errors['email'] = 'Некорректный email';
             }
+            $userModel = new UserModel();
+            if($userModel->find($data['login'])){
+                $errors['login'] = 'Login уже используется';
+            }
+            if($userModel->findByEmail($data['email'])){
+                $errors['email'] = 'Email уже зарегистрирован';
+            }
 
-        }
         return $errors;
     }
 }
