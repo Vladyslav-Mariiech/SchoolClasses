e40685bdca617fa81d8ebf04d80d46479b63e4f0
Stas0802 <stasshulga6@gamail.com>
2025-07-21 07:59:28 +0300

    add php

--- app/views/pages/index_index_page.php
+++ app/views/pages/index_index_page.php
@@ -11,15 +11,19 @@
         <form method="get" action="/index/registerPage/">
             <button class="register-btn">Реєстрація</button>
         </form>
+
         <form method="post" action="/auth/login">
+            <?php if(!empty($errors['common'])):?> <div><?=$errors['common']?></div> <?php endif;?>
             <h2>Вхід</h2>
-            <label>
+            <?php if(!empty($errors['login'])):?> <div><?=$errors['login']?></div> <?php endif;?>
+			<label>
                 Логін: <br>
-                <input type="text" name="login">
+                <input type="text" name="login" class="<?= isset($errors) ? 'error-input' : ''?>">
             </label><br>
+            <?php if(!empty($errors['password'])):?> <div><?=$errors['password']?></div> <?php endif;?>
             <label>
                 Пароль: <br>
-                <input type="password" name="password">
+                <input type="password" name="password" class="<?= isset($errors) ? 'error-input' : ''?>">
             </label><br><br>
             <button type="submit">Підтвердити</button>
             <button type="reset">Відмінити</button>
