414ecb213aecc91019da28bcab31ef0dd4d6daf6
Stas0802 <stasshulga6@gamail.com>
2025-07-21 07:54:55 +0300

    add method create and store and change method index

--- app/controllers/AssignmentsController.php
+++ app/controllers/AssignmentsController.php
@@ -2,9 +2,11 @@
 
 namespace app\controllers;
 
+use app\core\Redirect;
 use app\core\Session;
 use app\core\View;
 use app\models\AssignmentsModel;
+use app\services\AuthService;
 
 class AssignmentsController
 {
@@ -24,11 +26,48 @@ class AssignmentsController
      */
     public function index(): void
     {
-        $login = Session::getSession('user_login');
-        $ownerId = Session::getSession('user_id');
+        $login = AuthService::user()['login'];
+        $ownerId = AuthService::userId();
+        var_dump($ownerId);
         //TODO add errors or redirect
         $assignments = $this->assignmentsModel->all($ownerId);
-        $this->view->render('index_groupWhereTeacher', ['assignments' => $assignments, 'login' => $login]);
+        $this->view->render('index_groupWhereTeacher', [
+                'assignments' => $assignments,
+                'ownerId' => $ownerId, 'login' => $login,
+                ]);
     }
+
+    /**
+     * Page with form for create home work
+     * @return void
+     */
+    public function create(): void
+    {
+        $errors = Session::getSession('errors');
+        Session::deleteSession('errors');
+        $ownerId = AuthService::userId();
+        $classes = $this->assignmentsModel->getTeacherClasses($ownerId);
+        $this->view->render('index_createHomeWork', ['classes' => $classes, 'errors' => $errors]);
+    }
+
+    /**
+     * Save new home work
+     * @return void
+     */
+    public function store(): void
+    {
+        $classId = $_POST['class_id'];
+        $deadline = $_POST['deadline'];
+        $file = $_FILES['file']['name'];
+
+        if (!$classId || !$file) {
+            Session::setSession('errors', 'выберете файл и имя класса');
+            Redirect::redirect('/assignments/create');
+        }
+        // i dont no where redirect
+        $this->assignmentsModel->store($classId, $file, $deadline);
+        Redirect::redirect('/assignments/index');
+    }
+
 
 }
