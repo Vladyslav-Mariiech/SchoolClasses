565f36a0342fc09da0c9234f68e75037b61254c1
Stas0802 <stasshulga6@gamail.com>
2025-07-28 17:29:45 +0300

    refactor method getSubmission and adding method getByUserId, getByUserIdAndClass,getClassName

--- app/models/SubmissionModel.php
+++ app/models/SubmissionModel.php
@@ -14,27 +14,77 @@ class SubmissionModel extends BaseModel
     }
 
     /**
-     * Get submissions by user.
-     * @param int $id
-     * @return array
+     * Retrieves submission data for a user,
+     * optionally filtered by class.
+     * @param int $userId
+     * @param int|null $classId
+     * @return array|bool
      */
-    public function getByUserId(int $id): array
+    private function getSubmission(int $userId, int $classId = null): bool|array
     {
-        $stmt ="SELECT assignments.id, assignments.due_date, 
-                CASE WHEN ISNULL(submissions.grade) = 1 THEN '-' ELSE submissions.grade END AS grade, 
-                CASE 
-                    WHEN assignments.due_date >= NOW() THEN 'Active'
-                    ELSE 'Expired'
-                END AS status, submissions.path 
+
+        $stmt = "SELECT assignments.id AS assignment_id,
+                    assignments.due_date,
+                    CASE WHEN ISNULL(submissions.grade) = 1 THEN '-' ELSE submissions.grade END AS grade,
+                    CASE
+                        WHEN submissions.id IS NOT NULL AND submissions.grade > 0 THEN 'Checked'
+                        WHEN submissions.id IS NOT NULL THEN 'Submitted'
+                        WHEN assignments.due_date >= NOW() THEN 'Active'
+                        ELSE 'Expired'
+                    END AS status,
+                    submissions.path
             FROM assignments
             INNER JOIN classes ON assignments.class_id = classes.id
             INNER JOIN users_classes ON users_classes.class_id = classes.id
-            LEFT JOIN submissions 
-                ON submissions.assignment_id = assignments.id 
-                AND submissions.user_id = users_classes.user_id 
-            WHERE users_classes.user_id = ?
-            ORDER BY assignments.due_date;";
-        return $this->db->query($stmt, 'i', [$id]);
+            LEFT JOIN submissions
+                ON submissions.assignment_id = assignments.id
+                AND submissions.user_id = users_classes.user_id
+            WHERE users_classes.user_id = ?";
+
+        $types = "i";
+        $params = [$userId];
+        if($classId !== null){
+            $stmt .= " AND assignments.class_id = ?";
+            $types .= "i";
+            $params[] = $classId;
+        }
+        $stmt .= " ORDER BY assignments.due_date";
+        return $this->db->query($stmt, $types, $params);
+    }
+
+    /**
+     * Get all submissions for a specific user
+     * @param int $id
+     * @return array|bool
+     */
+    public function getByUserId(int $id): bool|array
+    {
+        return $this->getSubmission($id);
+    }
+
+    /**
+     * Get submissions for a specific user and class
+     * @param int $userId
+     * @param int $classId
+     * @return array|bool
+     */
+    public function getByUserIdAndClass(int $userId, int $classId): bool|array
+    {
+        return $this->getSubmission($userId, $classId);
+    }
+
+    /**
+     * Get class name
+     * @param int $classId
+     * @return mixed|null
+     */
+    public function getClassName(int $classId): mixed
+    {
+        $result = $this->db->query("SELECT name FROM classes WHERE id = ? ", "i", [$classId]);
+        if(!empty($result)){
+            return $result[0];
+        }
+        return null;
     }
 
     /**
@@ -45,10 +95,10 @@ class SubmissionModel extends BaseModel
      * @param int $grade
      * @return bool
      */
-    public function store(int $assignment_id, string $path, int $userId, int $grade = 0): bool
+    public function store(int $assignment_id, string $path, int $userId): bool
     {
         $stmt = "INSERT INTO submissions (assignment_id, submission_date, path, user_id, grade) VALUES(?, NOW(),?, ?, 0)";
-        return $this->db->query($stmt,'isii', [$assignment_id, $path, $userId, $grade]);
+        return $this->db->query($stmt,'isi', [$assignment_id, $path, $userId]);
     }
 
     /**
@@ -63,4 +113,5 @@ class SubmissionModel extends BaseModel
         $stmt = "UPDATE submissions SET grade = ? WHERE assignment_id = ? AND user_id = ?";
         return $this->db->query($stmt, 'iii', [$grade, $assignmentId, $userId]);
     }
+
 }
