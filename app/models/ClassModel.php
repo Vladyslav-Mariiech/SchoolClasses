071388ab769500396dec7a78baf84702e1f20c12
Stas0802 <stasshulga6@gamail.com>
2025-07-28 17:24:36 +0300

    add method findByLink

--- app/models/ClassModel.php
+++ app/models/ClassModel.php
@@ -11,7 +11,7 @@ class ClassModel extends BaseModel
      * @param int $ownerId
      * @return int|string
      */
-    public function add(string $name, string $link, int $ownerId)
+    public function add(string $name, string $link, int $ownerId): int|string
     {
         $this->db->query(
             'INSERT INTO classes (name, link, owner_id) VALUES (?,?,?)',
@@ -23,4 +23,19 @@ class ClassModel extends BaseModel
         
         return $lastId;
     }
+
+    /**
+     * Finds a class by its unique invite link.
+     * @param string $link
+     * @return bool|array|null
+     */
+    public function findByLink(string $link): bool|array|null
+    {
+        $result =  $this->db->query("SELECT id, name FROM classes WHERE link = ?", "s", [$link]);
+        if($result){
+            return $result[0];
+        }
+        return null;
+    }
+
 }
